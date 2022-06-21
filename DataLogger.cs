dd4bdf0a7de411c9cd127adbe7013807c7abb118
Riku Tanji <tatutoriato25@gmail.com>
2022-06-21 21:21:42 +0900

    first

---
+++ DataLogger.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class DataLogger : ILogger
+{
+    private const string format = "{0}";
+    private const string tagFormat = "{0}: {1}";
+
+    private static readonly ILogHandler defaultLogHandler = Debug.unityLogger.logHandler;
+
+    public ILogHandler logHandler { get; set; }
+    public bool logEnabled { get; set; }
+    public LogType filterLogType { get; set; }
+
+    public DataLogger()
+    {
+        logHandler = defaultLogHandler;
+        logEnabled = true;
+        filterLogType = LogType.Log;
+    }
+
+    private static string GetString(object message)
+    {
+        if (message == null)
+        {
+            return "Null";
+        }
+        var formattable = message as IFormattable;
+        if (formattable != null)
+        {
+            return formattable.ToString(null, CultureInfo.InvariantCulture);
+        }
+        else
+        {
+            return message.ToString();
+        }
+    }
+
+    public bool IsLogTypeAllowed(LogType logType)
+    {
+        if (!logEnabled) return false;
+
+        return (filterLogType <= logType);
+    }
+
+    public void Log(LogType logType, object message)
+    {
+        if (IsLogTypeAllowed(logType)) logHandler.LogFormat(logType, null, format, new object[] { GetString(message) });
+    }
+    
+    public void Log(LogType logType, object message, UnityEngine.Object context)
+    {
+        if (IsLogTypeAllowed(logType)) logHandler.LogFormat(logType, context, format, new object[] { GetString(message) });
+    }
+
+    public void Log(LogType logType, string tag, object message)
+    {
+        if (IsLogTypeAllowed(logType)) logHandler.LogFormat(logType, null, tagFormat, new object[] { tag, GetString(message) });
+    }
+
+    public void Log(LogType logType, string tag, object message, UnityEngine.Object context)
+    {
+        if (IsLogTypeAllowed(logType)) logHandler.LogFormat(logType, context, tagFormat, new object[] { tag, GetString(message) });
+    }
+
+    public void Log(object message)
+    {
+        if (IsLogTypeAllowed(LogType.Log)) logHandler.LogFormat(LogType.Log, null, format, new object[] { GetString(message) });
+    }
+
+    public void Log(string message)
+    {
+        if (IsLogTypeAllowed(LogType.Log)) logHandler.LogFormat(LogType.Log, null, format, new object[] { message });
+    }
+
+    public void Log<T>(IEnumerable<T> message)
+    {
+        if (IsLogTypeAllowed(LogType.Log)) logHandler.LogFormat(LogType.Log, null, format, new object[] { string.Join(", ", message) });
+    }
+
+    public void Log(string tag, object message)
+    {
+        if (IsLogTypeAllowed(LogType.Log)) logHandler.LogFormat(LogType.Log, null, tagFormat, new object[] { tag, GetString(message) });
+    }
+
+    public void Log(string tag, object message, UnityEngine.Object context)
+    {
+        if (IsLogTypeAllowed(LogType.Log)) logHandler.LogFormat(LogType.Log, context, tagFormat, new object[] { tag, GetString(message) });
+    }
+
+    public void LogError(object message)
+    {
+        if (IsLogTypeAllowed(LogType.Error)) logHandler.LogFormat(LogType.Error, null, format, new object[] { GetString(message) });
+    }
+
+    public void LogError(string tag, object message)
+    {
+        if (IsLogTypeAllowed(LogType.Error)) logHandler.LogFormat(LogType.Error, null, tagFormat, new object[] { tag, GetString(message) });
+    }
+
+    public void LogError(string tag, object message, UnityEngine.Object context)
+    {
+        if (IsLogTypeAllowed(LogType.Error)) logHandler.LogFormat(LogType.Error, context, tagFormat, new object[] { tag, GetString(message) });
+    }
+
+    public void LogException(Exception exception)
+    {
+        if (IsLogTypeAllowed(LogType.Exception)) logHandler.LogException(exception, null);
+    }
+
+    public void LogException(Exception exception, UnityEngine.Object context)
+    {
+        if (IsLogTypeAllowed(LogType.Exception)) logHandler.LogException(exception, context);
+    }
+
+    public void LogFormat(LogType logType, string format, params object[] args)
+    {
+        if (IsLogTypeAllowed(LogType.Log)) logHandler.LogFormat(LogType.Log, null, format, args);
+    }
+
+    public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+    {
+        if (IsLogTypeAllowed(LogType.Log)) logHandler.LogFormat(LogType.Log, context, format, args);
+    }
+
+    public void LogWarning(object message)
+    {
+        if (IsLogTypeAllowed(LogType.Warning)) logHandler.LogFormat(LogType.Log, null, format, new object[] { GetString(message) });
+    }
+
+    public void LogWarning(string tag, object message)
+    {
+        if (IsLogTypeAllowed(LogType.Warning)) logHandler.LogFormat(LogType.Log, null, tagFormat, new object[] { tag, GetString(message) });
+    }
+
+    public void LogWarning(string tag, object message, UnityEngine.Object context)
+    {
+        if (IsLogTypeAllowed(LogType.Warning)) logHandler.LogFormat(LogType.Log, context, tagFormat, new object[] { tag, GetString(message) });
+    }
+}
